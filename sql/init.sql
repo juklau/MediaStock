db7cd302681343f84849a5d5c4202aa9aa8fbd16
Plum Creative <denys.lyulchak333@gmail.com>
2025-10-20 12:32:59 +0200

    modification de la BDD, mise à jour

--- sql/init.sql
+++ sql/init.sql
@@ -1,15 +1,13 @@
 CREATE TABLE `Item`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `nom` TEXT NOT NULL,
-    `model` TEXT NOT NULL,
-    `qr_code` TEXT NOT NULL,
+    `nom` VARCHAR(50) NOT NULL,
+    `model` VARCHAR(50) NULL,
+    `qr_code` VARCHAR(255) NOT NULL,
     `état` ENUM('') NOT NULL,
-    `categorie_id` BIGINT NOT NULL,
-    `admin_id` BIGINT NOT NULL,
-    `statut` ENUM('') NOT NULL
+    `categorie_id` BIGINT NOT NULL
 );
 ALTER TABLE
-    `Item` ADD INDEX `item_admin_id_index`(`admin_id`);
+    `Item` ADD INDEX `item_categorie_id_index`(`categorie_id`);
 ALTER TABLE
     `Item` ADD INDEX `item_categorie_id_index`(`categorie_id`);
 CREATE TABLE `Pret`(
@@ -17,50 +15,57 @@ CREATE TABLE `Pret`(
     `item_id` BIGINT NOT NULL,
     `emprenteur_id` BIGINT NOT NULL,
     `date_sortie` DATE NOT NULL,
-    `date_retour_prévue` DATE NULL,
-    `date_retour_effective` DATE NOT NULL,
-    `note_debut` TEXT NOT NULL,
-    `note_fin` TEXT NOT NULL
+    `date_retour_prévue` DATE NOT NULL,
+    `date_retour_effective` DATE NULL,
+    `note_debut` VARCHAR(200) NOT NULL,
+    `note_fin` VARCHAR(200) NOT NULL,
+    `preteur_id` BIGINT NOT NULL
 );
 ALTER TABLE
-    `Pret` ADD INDEX `pret_item_id_emprenteur_id_index`(`item_id`, `emprenteur_id`);
+    `Pret` ADD INDEX `pret_item_id_emprenteur_id_preteur_id_index`(
+        `item_id`,
+        `emprenteur_id`,
+        `preteur_id`
+    );
 ALTER TABLE
     `Pret` ADD INDEX `pret_item_id_index`(`item_id`);
 ALTER TABLE
     `Pret` ADD INDEX `pret_emprenteur_id_index`(`emprenteur_id`);
+ALTER TABLE
+    `Pret` ADD INDEX `pret_preteur_id_index`(`preteur_id`);
 CREATE TABLE `Administrateur`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `login` TEXT NOT NULL,
-    `mot_de_passe_hash` TEXT NOT NULL
+    `login` VARCHAR(50) NOT NULL,
+    `mot_de_passe_hash` VARCHAR(100) NOT NULL
 );
 CREATE TABLE `emprunteur`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `emprunteur_nom` TEXT NOT NULL,
-    `emprunteur_prenom` TEXT NOT NULL,
-    `role_id` BIGINT NOT NULL,
+    `emprunteur_nom` VARCHAR(50) NOT NULL,
+    `emprunteur_prenom` VARCHAR(50) NOT NULL,
+    `role` ENUM('') NOT NULL,
     `formation_id` BIGINT NOT NULL
 );
+ALTER TABLE
+    `emprunteur` ADD INDEX `emprunteur_formation_id_index`(`formation_id`);
 CREATE TABLE `categorie`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `categorie` TEXT NOT NULL
+    `categorie` VARCHAR(50) NOT NULL
 );
 CREATE TABLE `Formation`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `formation` TEXT NULL
-);
-CREATE TABLE `Role`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `role` ENUM('') NOT NULL
+    `formation` VARCHAR(50) NULL
 );
 CREATE TABLE `sous_categorie`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `sous_categorie` TEXT NOT NULL,
+    `sous_categorie` VARCHAR(50) NOT NULL,
     `categorie_id` BIGINT NOT NULL
 );
 ALTER TABLE
     `sous_categorie` ADD INDEX `sous_categorie_categorie_id_index`(`categorie_id`);
 ALTER TABLE
-    `Item` ADD CONSTRAINT `item_admin_id_foreign` FOREIGN KEY(`admin_id`) REFERENCES `Administrateur`(`id`);
+    `sous_categorie` ADD INDEX `sous_categorie_categorie_id_index`(`categorie_id`);
+ALTER TABLE
+    `Pret` ADD CONSTRAINT `pret_preteur_id_foreign` FOREIGN KEY(`preteur_id`) REFERENCES `Administrateur`(`id`);
 ALTER TABLE
     `emprunteur` ADD CONSTRAINT `emprunteur_formation_id_foreign` FOREIGN KEY(`formation_id`) REFERENCES `Formation`(`id`);
 ALTER TABLE
@@ -71,6 +76,4 @@ ALTER TABLE
     `Pret` ADD CONSTRAINT `pret_item_id_foreign` FOREIGN KEY(`item_id`) REFERENCES `Item`(`id`);
 ALTER TABLE
     `sous_categorie` ADD CONSTRAINT `sous_categorie_categorie_id_foreign` FOREIGN KEY(`categorie_id`) REFERENCES `categorie`(`id`);
-ALTER TABLE
-    `emprunteur` ADD CONSTRAINT `emprunteur_role_id_foreign` FOREIGN KEY(`role_id`) REFERENCES `Role`(`id`);
     
