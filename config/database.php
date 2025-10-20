99e7823b47b20ab1dc6d4f35a1f2fd0324cff8a8
Klaudia <juklau83@gmail.com>
2025-10-20 09:56:54 +0200

    modification Docker-image

--- config/database.php
+++ config/database.php
@@ -2,20 +2,34 @@
 
 <?php
 
-  $host = getenv('DB_HOST') ?: 'mysql'; // 'mysql' est le nom du service dans docker-compose.yml
-  $db   = getenv('DB_NAME') ?: 'mediastock';
-  $user = getenv('DB_USER') ?: 'root';
-  $pass = getenv('DB_PASSWORD') ?: '';
-  // PDO utilise dsn (data source name) pour se connecter
-  $dsn  = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+    declare(strict_types=1);
 
-  $options = [
-    // pour gérer les erreurs, on choisit de lancer des exceptions
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    // les résultats sont renvoyés en tableaux associatifs (clés = noms de colonnes) => pas besoin d'indices numériques
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-  ];
+    //connexion PDO à la base applicative
 
-  return new PDO($dsn, $user, $pass, $options);
+    //function database(): PDO{
+
+        $host = getenv('DB_HOST') ?: 'mysql'; // 'mysql' est le nom du service dans docker-compose.yml
+        $db   = getenv('DB_NAME') ?: 'mediastock';
+        $user = getenv('DB_USER') ?: 'root';
+        // $user = getenv('DB_USER') ?: 'mediastock';
+        $pass = getenv('DB_PASSWORD') ?: '';
+
+        // PDO utilise dsn (data source name) pour se connecter
+        $dsn  = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+        $options = [
+
+          // pour gérer les erreurs, on choisit de lancer des exceptions
+          PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+
+          // les résultats sont renvoyés en tableaux associatifs (clés = noms de colonnes) => pas besoin d'indices numériques
+          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ];
+
+        return new PDO($dsn, $user, $pass, $options);
+
+    // }
+
+   
 
 ?>
---
+++ public/index.php
@@ -0,0 +1,5 @@
+
+<!-- temporaire -->
+
+<?php phpinfo();
+?>
