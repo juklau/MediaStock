58f266974394360227ffe9b2628005b86e3c260b
Klaudia <juklau83@gmail.com>
2025-11-05 16:43:48 +0100

    petit modif

--- public/auth_check.php
+++ public/auth_check.php
@@ -1,8 +1,8 @@
 <?php
-session_start();
+    session_start();
 
-if (!isset($_SESSION['username'])) {
-    header('Location: login.php');
-    exit();
-}
+    if (!isset($_SESSION['username'])) {
+        header('Location: login.php');
+        exit();
+    }
 ?>
